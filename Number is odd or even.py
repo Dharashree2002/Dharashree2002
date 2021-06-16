ae278a53127bb32961eec0fc4ac469b071e59c9e
Dharashree2002 <84570944+Dharashree2002@users.noreply.github.com>
2021-06-16 20:39:39 +0530

    Add files via upload

---
+++ Number is odd or even.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# Qno-WAP to check weather a number is positive,negative or zero
+num=int(input("Enter a number: "))
+print(f"{num} is positive") if num>0 else print(f"{num} is negative") if num<0 else print(f"number is {num} ")
+
+
+# In[ ]:
+
+
+
+
